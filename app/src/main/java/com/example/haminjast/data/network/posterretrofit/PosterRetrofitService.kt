61ac7d5585f4680782ed72cc38d21162e9b9fc1a
Mohammadreza Saemipour <mohammadrezasaemipour@gmail.com>
2023-11-17 21:00:11 +0330

    feat : connect PosterDetail to api

--- app/src/main/java/com/example/haminjast/data/network/posterretrofit/PosterRetrofitService.kt
+++ app/src/main/java/com/example/haminjast/data/network/posterretrofit/PosterRetrofitService.kt
@@ -1,8 +1,15 @@
 package com.example.haminjast.data.network.posterretrofit
 
 import com.example.haminjast.data.model.EntityResponse
+import com.example.haminjast.data.model.EntityResponseForPoster
+import com.example.haminjast.data.model.RequestBodyForAddPoster
+import okhttp3.RequestBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PosterRetrofitService {
@@ -16,4 +23,18 @@ interface PosterRetrofitService {
         @Query("state") state: String,
         @Query("special_type") specialType: String
     ): Response<EntityResponse>
+
+    @GET("posters/{posterId}")
+    suspend fun getPoster(
+        @Path("posterId") posterId: Int
+    ): Response<EntityResponseForPoster>
+
+
+    @POST("/api/v1/posters/authorize")
+    suspend fun createPoster(
+        @Header("authorization") authorization: String,
+        @Header("accept") acceptHeader: String,
+        @Header("Content-Type") contentTypeHeader: String,
+        @Body requestBody: RequestBodyForAddPoster
+    ): Response<EntityResponseForPoster>
 }
