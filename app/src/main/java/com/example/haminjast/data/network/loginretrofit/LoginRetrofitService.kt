446187d9643549df339a3683c4b928d3ae4a20ca
Mohammadreza Saemipour <mohammadrezasaemipour@gmail.com>
2023-12-01 16:46:28 +0330

    feat : add login with google

--- app/src/main/java/com/example/haminjast/data/network/loginretrofit/LoginRetrofitService.kt
+++ app/src/main/java/com/example/haminjast/data/network/loginretrofit/LoginRetrofitService.kt
@@ -2,8 +2,11 @@ package com.example.haminjast.data.network.loginretrofit
 
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Query
+
 interface LoginRetrofitService{
     //send OTP
     @POST("users/auth/otp/send")
@@ -17,6 +20,13 @@ interface LoginRetrofitService{
     suspend fun verifyOtpRequest(
         @Body otpRequest: VerifyOtpRequest
     ): Response<VerifyOtpResponse>
+
+    @GET("users/auth/google/login/android")
+    @Headers("accept: application/json")
+    suspend fun loginUserWithGoogle(
+        @Query("email") email: String
+    ): Response<VerifyOtpResponse>
+
 }
 data class OtpRequest(
     val username: String
