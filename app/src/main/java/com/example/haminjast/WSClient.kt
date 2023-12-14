4452b4ed3130587d28a8c2292eaa8020686b1f3b
mohammad <mo.darmanlou@gmail.com>
2023-12-14 23:54:47 +0330

    fix: Implement chat web socket

--- app/src/main/java/com/example/haminjast/WSClient.kt
+++ app/src/main/java/com/example/haminjast/WSClient.kt
@@ -1,5 +1,13 @@
 package com.example.haminjast
 
+import android.util.Log
+import com.example.haminjast.User.token
+import com.example.haminjast.data.model.MessageReceivedUpdate
+import com.example.haminjast.data.repository.ChatRepository
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
@@ -8,19 +16,14 @@ import okhttp3.WebSocketListener
 import okio.ByteString
 import java.util.concurrent.TimeUnit
 
-class WSClient : WebSocketListener() {
+class WSClient(private val chatRepository: ChatRepository) : WebSocketListener() {
 
-    companion object {
-        const val TOKEN =
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmVkIjoiMjAyMy0xMi0wNVQyMzoxMjoxNi4zNjI0NTg1NjNaIiwiaWQiOiJjOTE4MmRhNS0yNDQwLTQ5NTYtOGU3OC0yZmM5ODMxMTExZTEiLCJpc3N1ZWRBdCI6IjIwMjMtMTEtMjhUMjM6MTI6MTYuMzYyNDU4MTE2WiIsInJvbGUiOiJVc2VyIiwidXNlcklkIjoxLCJ1c2VybmFtZSI6IjA5MTk2MTkwNTMzIn0.KchbpVVHa8Zn5-26r9RmIBbuixEacVxZ4L3BsUlhfXI"
-    }
-
-    fun run(token: String = TOKEN): WebSocket {
+    fun run(): WebSocket {
         val client: OkHttpClient = OkHttpClient.Builder()
             .readTimeout(0, TimeUnit.MILLISECONDS)
             .build()
         val request: Request = Request.Builder()
-            .url("ws://localhost:8080/api/v1/chat/open-ws?token=$token") //TODO token
+            .url("wss://main-app.liara.run/api/v1/chat/open-ws?token=$token") //TODO token
             .build()
 
         val ws = client.newWebSocket(request, this)
@@ -32,25 +35,36 @@ class WSClient : WebSocketListener() {
     }
 
     override fun onOpen(webSocket: WebSocket, response: Response) {
+        Log.d("modar", "ws open")
 //        webSocket.send("Hello...")
 //        webSocket.send("...World!")
 //        webSocket.close(1000, "Goodbye, World!")
     }
 
     override fun onMessage(webSocket: WebSocket, text: String) {
+        Log.d("modar", "ws ${Thread.currentThread().name} onMessage1 $text");
+
+        CoroutineScope(Dispatchers.IO).launch {// TODO handle scope
+            val receivedMessage = Gson().fromJson(text, MessageReceivedUpdate::class.java)
+            chatRepository.onMessageReceived(receivedMessage)
+        }
+
         println("MESSAGE: $text")
     }
 
     override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+        Log.d("modar", "ws onMessage2");
         println("MESSAGE: " + bytes.hex())
     }
 
     override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+        Log.d("modar", "ws onClosing");
         webSocket.close(1000, null)
         println("CLOSE: $code $reason")
     }
 
     override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+        Log.d("modar", "ws onFailure ${t.message}");
         t.printStackTrace()
     }
 }
