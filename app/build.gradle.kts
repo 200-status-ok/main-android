9fd207ac9fd6c145d7fa6b79a42395009278a809
mohammad <mo.darmanlou@gmail.com>
2023-12-22 20:23:44 +0330

    feat: Implement swipe to refresh posters

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.2.0-alpha02")
+    implementation("eu.bambooapps:compose-material3-pullrefresh:1.0.0")
     implementation("androidx.compose.runtime:runtime")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.4")
     implementation("androidx.navigation:navigation-compose:2.7.4")
